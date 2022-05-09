18d706efa38f328686025c01cf2aba9eaf8ae8eb
Eric Hafvenstein <81645330+theFlexOne@users.noreply.github.com>
2022-05-09 16:02:11 -0500

    comit

--- client/src/pages/Home/components/LocationSection/LocationSection.js
+++ client/src/pages/Home/components/LocationSection/LocationSection.js
@@ -11,7 +11,7 @@ const LocationSection = () => {
           frameBorder="0"
           style={{ border: 0 }}
           referrerPolicy="no-referrer-when-downgrade"
-          src="https://www.google.com/maps/embed/v1/place?key=AIzaSyA0eAYPm-SK0fohxvpEhgXYJYS8vMWZW_A&q=place_id:ChIJhUtjDAediocR_uDCuz6WeMU"
+          // src="https://www.google.com/maps/embed/v1/place?key=KEY_HERE&q=place_id:ChIJhUtjDAediocR_uDCuz6WeMU"
           allowFullScreen
         ></iframe>
       </div>
